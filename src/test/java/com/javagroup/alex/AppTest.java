b3cf002ff249cefdc01f45e281b64d2c1792ea75
Olexii Riabokon <riabokon2078@gmail.com>
2015-07-05 08:12:27 +0300

    initial commit

---
+++ src/test/java/com/javagroup/alex/AppTest.java
@@ -0,0 +1,11 @@
+package com.javagroup.alex;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+  @Test
+  public void main() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
